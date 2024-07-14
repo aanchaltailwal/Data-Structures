cb4897398e6ba4b6daa9988a09c6e30d75e6cab6
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-14 18:56:24 +0530

    Create FirstClass.java

---
+++ FirstClass.java
@@ -0,0 +1,18 @@
+package DSA;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		//* Patter
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+
+	}
+
+}
