0df681702e6283d7d00e6f24466ab067f20d34d1
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-14 18:57:04 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,17 @@
+package DSA;
+
+import java.util.*;
+
+public class Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter our input");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();  //for single input
+	//	String name = sc.nextline();  //for whole line input 
+		System.out.println(name);
+
+	}
+
+}
