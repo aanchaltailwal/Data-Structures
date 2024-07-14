e54b6bbacd2f26965b2fb4d8dbb1f275ea63af22
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-14 18:57:47 +0530

    Create  OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,22 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the number");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		if(a % 2 == 0) {
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+
+	}
+
+}
