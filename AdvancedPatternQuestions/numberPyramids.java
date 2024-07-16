87478074c6ee1ba67d2f576e2fd00e17ad4f9fdc
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-16 17:37:26 +0530

    Create numberPyramids.java

---
+++ AdvancedPatternQuestions/numberPyramids.java
@@ -0,0 +1,22 @@
+package AdvancedPatterns;
+
+public class numberPyramids {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =5;
+		for(int i = 1; i <= n; i++) {
+			//spaces
+			for(int j = 1; j <= n-i; j++) {
+				System.out.print(" ");
+			}
+			//numbers
+			for(int j = 1; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
