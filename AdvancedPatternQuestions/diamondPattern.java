8787f73063512ac13ef76e9044269bf0d131aef3
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-17 15:12:42 +0530

    Create diamondPattern.java

---
+++ AdvancedPatternQuestions/diamondPattern.java
@@ -0,0 +1,37 @@
+package AdvancedPatterns;
+
+public class diamondPattern {
+
+    public static void main(String[] args) {
+        int n = 4; // Number of rows (adjust as needed)
+        
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
