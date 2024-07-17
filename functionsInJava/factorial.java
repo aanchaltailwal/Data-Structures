b710820d2e66299d598746b8dfed88d1c985434e
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-17 17:27:32 +0530

    Update factorial.java

--- functionsInJava/factorial.java
+++ functionsInJava/factorial.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class factorial {
 	public static void printFactorial(int n) {
+		if(n <=0) {
+			System.out.println("Invalid no.");
+			return;
+		}
 		int factorial = 1;
 		for(int i = n; i >=1; i--) {
 			factorial = factorial * i;
