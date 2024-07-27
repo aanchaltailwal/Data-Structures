a5c76b0d284fc25d24a654b250ae625e0ef3b613
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-27 18:21:20 +0530

    Create concatenatio.java

---
+++ Strings/concatenatio.java
@@ -0,0 +1,15 @@
+package string;
+
+public class concatenation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fName = "Aanchal";
+		String lName = "Tailwal";
+		String fullName = fName + " " + lName;
+		System.out.println(fullName);
+		
+
+	}
+
+}
