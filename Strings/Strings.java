a8aa9f6be889d109de7d7e5d841943b161e8f4ab
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-27 18:15:53 +0530

    Update and rename Variables.java to Strings.java

---
+++ Strings/Strings.java
@@ -0,0 +1,15 @@
+package string;
+
+import java.util.Scanner;
+
+public class ques1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.println("Your name is:" + name);
+
+	}
+
+}
