46f9103ca3dba300bf9d93aef78de2f16e4ffb4d
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-27 18:37:38 +0530

    Create subString.java

---
+++ Strings/subString.java
@@ -0,0 +1,13 @@
+package string;
+
+public class subString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = "My name is Aanchal";
+		String name = sentence.substring(11);
+		System.out.println(name);
+
+	}
+
+}
