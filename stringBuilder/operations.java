5bdeaa74ae6db8fa31b031fa961fa6436a924dcd
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-07-28 16:32:11 +0530

    Update and rename Variables.java to operations.java

---
+++ stringBuilder/operations.java
@@ -0,0 +1,11 @@
+package stringBuilder;
+
+public class operations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder("Aanchal");
+		System.out.println(sb);
+	}
+
+}
