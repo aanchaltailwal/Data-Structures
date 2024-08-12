6249deac643786abf8fd4ed59df4168e68353197
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-08-12 11:06:04 +0530

    .

---
+++ Backtracking/Ques1.java
@@ -0,0 +1,20 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class Ques1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter Variable a");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println("Enter Variable b");
+		int b = sc.nextInt();
+		int sum = a + b;
+		System.out.println(sum);
+		
+
+	}
+
+}
