8529eec147f4412fdd7e046ef4940df565538ce0
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-08-25 16:41:16 +0530

    Create inheritance.java

---
+++ OOPS/inheritance.java
@@ -0,0 +1,19 @@
+package Oops;
+
+class Shape{
+	String color;
+}
+class Triangle extends Shape{
+	
+}
+
+public class inheritance {
+
+	public static void main(String[] args) {
+		Triangle t1 = new Triangle();
+		t1.color = "red";
+		
+
+	}
+
+}
