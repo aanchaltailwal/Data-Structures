7723040a548a138701ed4b731ef280d4c57b5471
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2024-08-25 20:10:03 +0530

    Create publicAccessModifier.java

---
+++ OOPS/publicAccessModifier.java
@@ -0,0 +1,15 @@
+package Oops;
+
+class Account{
+	public String name; //public access modiefiers
+	
+}
+public class accessModifiers {
+
+	public static void main(String[] args) {
+		Account account1 = new Account();
+		account1.name = "apna college";
+
+	}
+
+}
